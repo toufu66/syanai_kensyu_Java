eff2c5c9ce12701807be3fd78d66490276392318
edu01 <edu01@EDUPC01.n00d01.kis.co.jp>
2020-04-22 14:56:26 +0900

    1~6までの乱数生成、１００回動かしてテスト

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,16 @@ public class Main {
 		int sansu = 75;
 		int num1 = 3;
 		int num2 = 5;
+		int r = new java.util.Random().nextInt(6) + 1;
 		out.printf("今日は%d月%d日です\n",month,day);
 		out.printf("円周率は%.2fです\n",Math.PI);
 		out.print("C:\\に\"フォルダ\"を作成\n");
 		out.printf("%dを3で割ったあまりは%dです\n", num,num%3);
 		out.printf("平均点は%.2f です\n", (kokugo+sansu)/2.);
-		out.printf("num1:%dとnum2:%dの足し算の結果:%d",num1,num2,num1+num2 );
+		out.printf("num1:%dとnum2:%dの足し算の結果:%d\n",num1,num2,num1+num2 );
+		for (int i = 0; i <100; i++) {
+			r = new java.util.Random().nextInt(6) + 1;
+			out.printf("%d  ",r);
+		}
 	}
 }
