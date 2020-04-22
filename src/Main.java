d1bd872a6c4f5b3dad81e37d2921bbbef9607b43
edu01 <edu01@EDUPC01.n00d01.kis.co.jp>
2020-04-22 13:19:41 +0900

    init data

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import static java.lang.System.*;
+public class Main {
+	public static void main(String[] args) {
+		int month = 4;
+		int day = 22;
+		out.printf("今日は%d月%d日です\n",month,day);
+		out.printf("円周率は%.2fです",Math.PI);
+	}
+}
