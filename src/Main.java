13a93003680d6618b4abdc6caf12eeccfb12c4ab
edu01 <edu01@EDUPC01.n00d01.kis.co.jp>
2020-04-22 14:19:32 +0900

    あまりの計算と平均点をだす計算を追記

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,13 @@ public class Main {
 	public static void main(String[] args) {
 		int month = 4;
 		int day = 22;
+		int num = 10;
+		int kokugo = 70;
+		int sansu = 75;
 		out.printf("今日は%d月%d日です\n",month,day);
-		out.printf("円周率は%.2fどぇす",Math.PI);
+		out.printf("円周率は%.2fです\n",Math.PI);
+		out.print("C:\\に\"フォルダ\"を作成\n");
+		out.printf("%dを3で割ったあまりは%dです\n", num,num%3);
+		out.printf("平均点は%.2f です", (double)(kokugo+sansu)/2);
 	}
 }
