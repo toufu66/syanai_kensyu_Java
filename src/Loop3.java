021e7ae1c88d1614b55a4c9ffb1e3a8f746b2ebf
edu01 <edu01@EDUPC01.n00d01.kis.co.jp>
2020-04-23 11:30:31 +0900

    名前間違えました

---
+++ src/Loop3.java
@@ -0,0 +1,44 @@
+import static java.lang.System.*;
+
+import java.util.Scanner;
+
+public class Loop3 {
+public static void main(String[] args) {
+
+	Scanner scan = new Scanner(System.in);
+
+	for (int i =0;i<5;i++) {
+		out.print("こんにちは ");
+	}
+	out.print("\n");
+
+	for (int i =0;i<5;i++) {
+
+		out.printf("%d ",i+1);
+	}
+
+	out.print("\n");
+	out.print("数字を入力してください");
+	int mx = scan.nextInt();
+
+	for (int i =0;i<mx;i++) {
+
+		out.printf("%d ",i+1);
+	}
+	out.print("\n");
+	for (int i =1;i<=10;i++) {
+		if(i%2==0) {
+			out.printf("%d ",i);
+		}
+	}
+
+	out.print("\n");
+
+	for (int i = 5; i > 0;i--) {
+
+		out.printf("%d ",i);
+
+	}
+
+}
+}
