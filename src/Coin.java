2669767093caae9e2ac343f1e54fe989e1c0ad30
edu01 <edu01@EDUPC01.n00d01.kis.co.jp>
2020-04-23 16:28:15 +0900

    みための修正

--- src/Coin.java
+++ src/Coin.java
@@ -6,10 +6,10 @@ public class Coin {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int[] coins = {500,100,50,5,1};
+		int[] coins = {500,100,50,10,5,1};
 		int sum = 0;
 		for (int i:coins) {
-			out.printf("%d円玉は何枚？..... : ",i);
+			out.printf("%3d円玉は何枚？..... : ",i);
 			sum += i*scan.nextInt();
 		}
 		out.printf("合計%d円",sum);
