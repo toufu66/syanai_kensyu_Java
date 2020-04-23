63180bb809b8234426a39e98150d56192ac5381c
edu01 <edu01@EDUPC01.n00d01.kis.co.jp>
2020-04-23 16:23:52 +0900

    えんしゅう

---
+++ src/Coin.java
@@ -0,0 +1,19 @@
+import static java.lang.System.*;
+
+import java.util.Scanner;
+
+public class Coin {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int[] coins = {500,100,50,5,1};
+		int sum = 0;
+		for (int i:coins) {
+			out.printf("%d円玉は何枚？..... : ",i);
+			sum += i*scan.nextInt();
+		}
+		out.printf("合計%d円",sum);
+		scan.close();
+	}
+
+}
