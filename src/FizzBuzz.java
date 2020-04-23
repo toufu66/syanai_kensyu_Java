772951ebd9a7b6c5e3639030ee9fae8db2f9e6f2
edu01 <edu01@EDUPC01.n00d01.kis.co.jp>
2020-04-23 16:57:11 +0900

    インデント修正

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,18 +4,18 @@ import static java.lang.System.*;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-for (int i =1;i <=30;i++) {
+		for (int i =1;i <=30;i++) {
 
-	if((i%3 == 0)&&(i%5 == 0)) {
-		out.println("FizzBuzz!!");
-	}else if( (i%3==0)) {
-		out.println("Fizz!!");
-	}else if( (i%5 == 0)) {
-		out.println("Buzz!!");
-	}else {
-		out.println(i);
-	}
-}
+			if((i%3 == 0)&&(i%5 == 0)) {
+				out.println("FizzBuzz!!");
+			}else if( (i%3==0)) {
+				out.println("Fizz!!");
+			}else if( (i%5 == 0)) {
+				out.println("Buzz!!");
+			}else {
+				out.println(i);
+			}
+		}
 
 	}
 
