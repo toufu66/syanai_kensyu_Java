d6a04d2c17eceeebfe17a09a16c53ae0faa5674f
toufu66 <swallow.snoopy66@gmail.com>
2020-04-24 14:54:03 +0900

    add hour method

--- src/Method.java
+++ src/Method.java
@@ -3,11 +3,12 @@ public class Method {
     public static void main(String[] args){
         int num = 3;
         if (evenNum(num)){
-            out.print("偶数");
+            out.println("偶数");
         }else{
-            out.print("奇数");
+            out.println("奇数");
         }
         //out.print(evenNum(num));
+        out.print(hour(12));
     }
 
     public static boolean evenNum(int num){ 
@@ -15,4 +16,16 @@ public class Method {
         return (num%2 == 0);
 
     }
+    public static String hour(int num){ 
+        //if Number is even Number, return True
+       if (num >24){
+           return "Error";
+       } else if(num/12 == 0){
+           return "午前";
+       }else {
+           return "午後";
+    }
+
+    }
+    
 }
