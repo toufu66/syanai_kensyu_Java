6bf1f46b844614801c8884ceaeca1e4a764a7372
toufu66 <swallow.snoopy66@gmail.com>
2020-04-24 14:03:47 +0900

    menseki comit sitemita

--- src/Menseki.java
+++ src/Menseki.java
@@ -1,33 +1,21 @@
 import static java.lang.System.*;
 public class Menseki {
-	public static void main(String[] args) {
-		int r=10;
+	public static void main(final String[] args) {
+		final int r = 10;
 		out.print("1問目  ");
 		out.println(circleArea(r));
 		out.print("2問目  ");
-		for (int i=1;i <=5; i++) {
+		for (int i = 1; i <= 5; i++) {
 			out.println(circleArea(i));
 		}
-
 		out.print("3問目  ");
-		int[] rs= {3,8,20};
-		for(int i:rs) {
+		final int[] rs = { 3, 8, 20 };
+		for (final int i : rs) {
 			out.println(circleArea(i));
-
 		}
 	}
 
-	public static double circleArea(int r) {
+	public static double circleArea(final int r) {
 		return r*r*Math.PI;
-	}
-
-
-
-
-
-
-
-
-
-
+	}	
 }
