4b79ab8de8819059c5cc6239ebe7e81a67b11f94
toufu66 <swallow.snoopy66@gmail.com>
2020-04-27 16:47:16 +0900

    結果にコミットする

--- src/seiseki/St.java
+++ src/seiseki/St.java
@@ -6,7 +6,7 @@ public class St {
     int mt;
     int sya;
     void show(){
-        System.out.printf("%s 国語:%d点, 算数:%d点, 社会:%d点",this.name,this.koku,this.mt,this.sya);        
+        System.out.printf("%s 国語:%d点, 算数:%d点, 社会:%d点\n",this.name,this.koku,this.mt,this.sya);        
     }
     int sum(){
         return(this.koku + this.mt + this.sya);
